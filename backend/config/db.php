a37d147c843e21bc025ab87f747d0769375db847
surya-dhanvi <bajjangisuryakumar@gamil.com>
2025-11-18 00:14:56 +0530

    added db.php file in /config

---
+++ backend/config/db.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+try {
+    $pdo = new PDO(
+        "mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'],
+        $_ENV['DB_USER'],
+        $_ENV['DB_PASS']
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die(json_encode([
+        "status" => "error",
+        "message" => "DB connection failed",
+        "error" => $e->getMessage()
+    ]));
+}
